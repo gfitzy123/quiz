3e7d9f5ba663bfb134c58e33dd662c0910fa732a
Garrett <gfitzgerald123@gmail.com>
2015-02-25 08:08:56 -0600

    first commit

---
+++ app/app.js
@@ -0,0 +1,82 @@
+var questionList2 = [
+{
+	question: "Are you ready to begin?",
+	answer:"yes"
+},
+{	
+	question: "On which fictional continent does the story take place?",
+	answer: "westeros"
+},
+{	
+	question: "What is the infamous nickname of Jaime Lannister?",
+	answer: "kingslayer"
+},
+{	
+	question: "What is the name of the home of House Stark?",
+	answer: "winterfell"
+},	
+{	
+	question: "What is the first name of the character some know as the Imp?",
+	answer: "tyrion"
+}];
+
+
+
+$("#submitButton").click(function(){
+
+
+
+
+});	
+
+
+function questionLooper () {
+	for (var in questionList2)
+		//put key in element
+		// put text box in dom
+		// put button in dom
+}
+
+button click event
+//check if button.id = answer
+
+/*TODO:
+* Write a function that loops over all of your questions, puts them
+* in a dom element(<p></p>), and adds them to the dom, along with a text box
+* element and a button.
+* Give each of those text boxes and buttons an id with the number of the question
+* you are on.  Tip: Use concatenation - "question" + i
+
+* Add an on click event for each of the buttons to look at the corresponding text box
+* and check for the answer.
+
+
+
+
+
+
+*/
+
+
+
+
+
+	//answerOne = $("#answer").val();
+	//begin at element, and increment element every time so this becomes a loop
+// if answer.val is value of [0], alert you are correct, and put next array element on page
+
+
+
+
+
+
+ 
+
+//when correct answer is inputed, hide HTML and show next 
+
+//create an if function for every queston, but if user enters answer to another question?
+
+//if user enters in correct answer, append HTML with new question, etc.
+
+//answer equals value of property, produce alert that says "correct, here is your next question", 
+
